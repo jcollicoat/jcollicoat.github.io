cbfec9d2caa8fa7c00d38c11b367537c8d6711e8
Joseph Collicoat <jcollicoat@gmail.com>
2018-05-19 12:26:21 +1200

    Fix usa-graphic-design directory
    Fix usa-graphic-design directory.

--- projects/assets/interactive/usa-graphic-design/javascript.js
+++
@@ -1,7 +0,0 @@
-function resOpen() {
-    document.getElementById("resdropdown").style.height = "400px";
-}
-
-function resClose() {
-    document.getElementById("resdropdown").style.height = "0px";
-}
